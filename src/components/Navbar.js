09544340f65bb8c00be95029159dd0f14988f0d3
Alexander Holm <alho0014@student.hv.se>
2022-06-29 16:03:19 +0200

    Dropdown-ikon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,24 @@ import React, { useState } from "react";
 import "./Navbar.css"
 import { HiMenu, HiOutlineX } from "react-icons/hi";
 import {IoTriangleSharp} from "react-icons/io5"
+import {BsTriangleFill} from "react-icons/bs"
 
 export default function Navbar(props){
     const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
 
-    const NavItem = ({item}) => (
-        <a key={item.title} href={item.link} className="nav-item" >
-            {item.title}
+    const NavItem = ({href, className, children}) => (
+        <a href={href} className={className} onClick={() => setIsHamburgerOpen(false)} >
+            {children}
         </a>
     );
     const Dropdown = ({item}) => (
         <div className="dropdown">
-            <NavItem item={item} />
+            <NavItem href={item.link} >
+                <span>{item.title}</span>
+                <BsTriangleFill className="dropdown-arrow" />
+            </NavItem>
             <div className="dropdown-content">
-                {item.dropdown.map(item => <NavItem item={item} key={item.title} /> )}
+                {item.dropdown.map(item => <NavItem href={item.link} key={item.title}>{item.title}</NavItem> )}
             </div>
         </div>
     );
@@ -28,14 +32,14 @@ export default function Navbar(props){
                 : <HiMenu />
                 }
             </button>
-            {isHamburgerOpen ? <IoTriangleSharp className="triangle"/> : null}
-            <div className={`items-container ${isHamburgerOpen ? "hamburgerOpen" : ""}`}>
+            {isHamburgerOpen && <IoTriangleSharp className="triangle"/> }
+            <div className={`items-container ${isHamburgerOpen && "hamburgerOpen"}`} >
                
-                <a href="#top" className={props.showTopButton === true ? "nav-item toTopButton active" : "nav-item toTopButton"}>🡅</a>
+                <NavItem href="#" className={`toTopButton ${props.showTopButton && "active"}`}>🡅</NavItem>
                 {props.items.map(item => {
                     if("dropdown" in item)
                         return <Dropdown item={item} key={item.title} />
-                    else return <NavItem item={item} key={item.title} />
+                    else return <NavItem href={item.link} key={item.title}>{item.title}</NavItem>
                 })}
             </div>
         </nav>
